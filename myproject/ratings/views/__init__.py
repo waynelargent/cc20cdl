ff6ff841004266170dd2df5847fca8b9b7e4a835
KhaywithanH <S00737019@acad.tri-c.edu>
2025-12-11 20:31:07 -0500

    Updated __init__py
    Moved Attendance and ELDT to likes 2 and 3

--- myproject/ratings/views/__init__.py
+++ myproject/ratings/views/__init__.py
@@ -1,10 +1,9 @@
 #12/10/2025
-from .attendance_views import attendance
-from .eldt_views import eldt_and_score_sheet
+from .attendance_views import attendance, instr_list_attendance, instr_edit_attendance
+from .eldt_views import eldt_and_score_sheet,instr_list_eldt, instr_edit_eldt
 from .pre_trip_views import pre_trip_insp, instr_list_pre_trip, instr_edit_pre_trip
 from .backing_skills import backing_skills
 from .road_skills import road_skills
 from .narrative_views import view_my_ratings, narrative
-from .attendance_views import instr_list_attendance, instr_edit_attendance
-from .eldt_views import instr_list_eldt, instr_edit_eldt
+
 
