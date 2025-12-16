b1d0ee7a595d8b7b85b16aa70ce1a08c6e7f9969
AGW-12 <austinwirtz@outlook.com>
2025-12-15 19:57:50 -0500

    Forms.py Update
    Updated Forms.py and backing skills form

--- myproject/ratings/views/backing_skills.py
+++ myproject/ratings/views/backing_skills.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, user_passes_test
 from .. import forms
+from django.contrib.auth.models import User, Group  # for student filter
 
 from ..models import BackingSkills
 
@@ -13,15 +14,26 @@ def backing_skills(request):
             newrating = form.save(commit = False)
             newrating.instructor = request.user
             newrating.save()
-            return redirect('/')
+            return redirect('/ratings/instr-list-backing-skills/')
     else:
         form = forms.CreateBackingSkills()
     return render(request,'ratings/backing_skills.html', {'form': form})  
 
 
 def instr_list_backing_skills(request):
+    students = User.objects.filter(groups__name='students')
+    selected_student_id = request.GET.get('student_id')
+    
     ratings = BackingSkills.objects.all().order_by('-rating_date')
-    return render(request, 'ratings/instr_list_backing_skills.html', {'ratings': ratings})
+    if selected_student_id:
+        ratings = ratings.filter(student_id=selected_student_id)
+
+    context = {
+        'students': students,
+        'ratings': ratings,
+        'selected_student_id': selected_student_id,
+    }
+    return render(request, 'ratings/instr_list_backing_skills.html', context)
 
 def instr_edit_backing_skills(request, pk):
     # 1. Fetch the existing record using the ID (pk) from the URL
